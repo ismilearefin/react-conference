ff963dbb20338c32744a85839a5b161c25af7318
ismilearefin <arefinismile@gmail.com>
2023-09-30 22:08:22 +0600

    Sponsors data fetched

--- src/app/conference/[id]/sponsors/page.js
+++ src/app/conference/[id]/sponsors/page.js
@@ -1,7 +1,25 @@
+"use client"
+import { dataQueries } from '@/graphQL/queries';
+import { useQuery } from '@apollo/client';
+import client from '../../../../graphQL/apollo';
 import React from 'react'
+import DetailCard from '@/components/detailCard/DetailCard';
 
-export default function Sponsors() {
+export default function Sponsors({params}) {
+  const id = params.id;
+    const { loading, error, data } = useQuery(dataQueries.GET_SPONSOR_BY_ID, {
+      client,
+      variables: { id },
+    });
+    if (loading) return "Loading...";
+    if (error) return `Error: ${error.message}`;
+    console.log(data)
+    const sponsors = data.conference.sponsors
   return (
-    <div>sponsors</div>
+    <div>
+      {
+        sponsors.map((organizer,i) => <DetailCard key={i} organizer={organizer}></DetailCard>)
+      }
+    </div>
   )
 }
--- src/graphQL/queries.js
+++ src/graphQL/queries.js
@@ -47,11 +47,25 @@ query GetConferenceById($id: ID!) {
   }
 }
 `;
+const GET_SPONSOR_BY_ID = gql`
+query GetConferenceById($id: ID!) {
+  conference(id: $id) {
+    sponsors{
+      name
+      about
+      image {
+        url
+      }
+    }
+  }
+}
+`;
 
 
 export const dataQueries = {
   GET_CONFERENCES_DATA,
   GET_CONFERENCE_BY_ID,
   GET_ORGRANIZER_BY_ID,
-  GET_SPEAKERS_BY_ID
+  GET_SPEAKERS_BY_ID,
+  GET_SPONSOR_BY_ID
 }
